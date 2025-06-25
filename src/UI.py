7abab21b2116392ae1411d76df55b2d375bfd376
Keens255 <tukaken25r@gmail.com>
2025-06-25 23:09:24 +0900

    UI.py追加（未実装）

---
+++ src/UI.py
@@ -0,0 +1,16 @@
+class UICore:
+    def __init__(self):
+        pass
+    def update(self):
+        #ここにUIステータス更新機能を追加します。
+        #(例)キャラクターのHP→UI表示用データへの反映
+        #子クラスで実装してください。
+        pass
+    def draw(self):
+        #表示用メソッド
+        pass
+
+
+class UIObj:
+    def __init__(UICore):   
+        pass
